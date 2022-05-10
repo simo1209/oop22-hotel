8043e3d5a1e4f575d57ef07ed200d03984d2ce3a
simo1209 <simeongeorgiev09@gmail.com>
2022-05-11 00:41:48 +0300

    Shell Hotel class definitions

---
+++ include/ShellHotel.hpp
@@ -0,0 +1,35 @@
+#include <fstream>
+
+#include "Hotel.hpp"
+
+#ifndef __SHELL_HOTEL_HPP
+#define __SHELL_HOTEL_HPP
+
+const size_t MAX_FILE_NAME = 100;
+
+class ShellHotel : public Hotel
+{
+    char *roomsFileName;
+    char *reservationsFileName;
+
+    std::fstream roomsFile;
+    std::fstream reservationsFile;
+
+    void setRoomsFileName(char const *roomsFileName);
+    void setReservationsFileName(char const *reservationsFileName);
+
+    void loadRooms();
+    void loadReservations();
+
+public:
+    ShellHotel(char const *roomsFileName, char const *reservationsFileName);
+    ~ShellHotel();
+
+    // void createReservation(Date &start, Date &end, Room &room, char const * note, char const *reservatorName = "");
+    // void listFreeRooms(Date &date);
+    // void releaseRoom(unsigned roomId);
+    // void reservationInquiry(Date &start, Date &end);
+    // Room findOpenRoom(Date start, Date end, unsigned bedCount);
+};
+
+#endif
